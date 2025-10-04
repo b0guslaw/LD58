ac3283873e800a6f6314dcf8b45b9332a241c212
hdathert <hankatherton@warmoverdrive.com>
2025-10-04 15:39:15 -0700

    completes barebones game loop

--- neighborhood.gd
+++ neighborhood.gd
@@ -6,15 +6,24 @@ extends Node3D
 
 func _ready():
 	ScreenFade.instant_fill()
+	TrashUi.set_up_trash_ui()
 	await get_tree().process_frame
 	ScreenFade.fade_in()
 	
 	if player:
 		player.player_died.connect(_on_player_died)
+	TrashUi.all_garbage_collected.connect(_on_game_won)
 
 func _on_player_died():
-	var tree = get_tree()
-	await tree.create_timer(reset_wait_time).timeout
+	print("oof") # set up "game won" or "game over" ui
+	await get_tree().create_timer(reset_wait_time).timeout
 	ScreenFade.fade_out()
 	await ScreenFade.fade_out_complete
-	tree.reload_current_scene()
+	get_tree().reload_current_scene()
+
+func _on_game_won():
+	print("yay") # set up "game won" or "game over" ui
+	await get_tree().create_timer(reset_wait_time).timeout
+	ScreenFade.fade_out()
+	await ScreenFade.fade_out_complete
+	get_tree().reload_current_scene()
--- trash_ui.gd
+++ trash_ui.gd
@@ -1,11 +1,19 @@
 extends CanvasLayer
 
+signal all_garbage_collected
+
 @onready var trash_counter: Label = $TrashCounter
 
-var trash_total: int = 10 # TODO, make this dynamic based on trash in scene
+var trash_total: int = 0 # this is dynamically updated to count all garbage
 
 func _ready():
-	update_trash_ui(0)
+	set_up_trash_ui()
 
 func update_trash_ui(trash_number: int):
 	trash_counter.text = "Trash %d / %d" % [trash_number, trash_total]
+	if trash_number == trash_total:
+		all_garbage_collected.emit()
+
+func set_up_trash_ui():
+	trash_total = get_tree().get_nodes_in_group("interactables").size()
+	update_trash_ui(0)
