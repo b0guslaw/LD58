6a4de5153e17ad80943fedc68796388675de3f64
Ralph Dworzanski <ralph.dworzanski@gmail.com>
2025-10-04 22:07:55 +0200

    Make interactables one shot

--- interactable.gd
+++ interactable.gd
@@ -1,9 +1,12 @@
 extends StaticBody3D
 
 @export var interaction_time: float = 2.0
+@export var empty_message_duration: float = 2.0  # How long "Empty" shows
 
 var interaction_timer: Timer
+var empty_message_timer: Timer
 var interacting_player: Node3D = null
+var can_loot: bool = true
 
 @onready var timer_label: Label3D = $Label3D
 
@@ -12,6 +15,11 @@ func _ready() -> void:
 	interaction_timer.one_shot = true
 	interaction_timer.timeout.connect(_on_interaction_timer_timeout)
 	add_child(interaction_timer)
+
+	empty_message_timer = Timer.new()
+	empty_message_timer.one_shot = true
+	empty_message_timer.timeout.connect(_on_empty_message_timeout)
+	add_child(empty_message_timer)
 	
 	# Hide label initially
 	if timer_label:
@@ -29,12 +37,20 @@ func _process(delta: float) -> void:
 		timer_label.modulate = color
 
 func start_interaction(player: Node3D) -> void:
+	if can_loot == false: # Show empty message
+		timer_label.visible = true
+		timer_label.text = "Empty"
+		timer_label.modulate = Color.GRAY
+		empty_message_timer.start(empty_message_duration)
+		return
+	
 	interacting_player = player
 	interaction_timer.start(interaction_time)
 	
 	if timer_label:
 		timer_label.visible = true
 		timer_label.text = str(ceil(interaction_time))
+		timer_label.modulate = Color.RED
 
 func cancel_interaction() -> void:
 	interaction_timer.stop()
@@ -47,6 +63,13 @@ func _on_interaction_timer_timeout() -> void:
 	if interacting_player != null and interacting_player.has_method("on_interaction_complete"):
 		interacting_player.on_interaction_complete(self)
 		
+		can_loot = false # cant be looted again
+		
 		if timer_label:
 			timer_label.visible = false
+		
 		interacting_player = null
+
+func _on_empty_message_timeout() -> void:
+	if timer_label:
+		timer_label.visible = false
